d353eed8fdc2e0afc36211914d91f45ceb5806e3
mohamed behnas <behna@DESKTOP-T1K4JBI.(none)>
2022-07-10 10:41:27 -0400

    add test component

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,3 +1,4 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -6,17 +7,13 @@ import About from '..';
 afterEach(cleanup);
 
 describe('About component', () => {
-    //renders About test
-    //first test
-    it('renders', ()=>{
-        render(<About />);
-    });
-
-    //second test
-    it('matches snapshot DOM node structure', () => {
-        // render About
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-      });
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
     
-  })
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
